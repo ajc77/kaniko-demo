d46ac12a2e81e7f64d07cbbcac4ecf5f0ddb1165
Andrew Cowan <andycowan@gmail.com>
2022-03-04 09:37:22 +1100

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		from = r.URL.String()
 	}
 	if from != "/favicon.ico" {
-		log.Printf("title: %s\n", "Kaniko demo")
+		log.Printf("title: %s\n", "Andy's Kaniko demo ", currentTime.Format("2006-01-02 15:04:05"))
 	}
 
 	fmt.Fprintf(w, "Built by Kaniko\n")
